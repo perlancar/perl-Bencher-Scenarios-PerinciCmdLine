ed6a8b8f36ea92c8ee0fdc8bcac64a1b8a51e696
perlancar (@pc-office) <perlancar@gmail.com>
2016-01-07 18:32:42 +0700

    Release v0.01

--- lib/Bencher/Scenario/PerinciCmdLine/Completion.pm
+++ lib/Bencher/Scenario/PerinciCmdLine/Completion.pm
@@ -6,33 +6,83 @@ package Bencher::Scenario::PerinciCmdLine::Completion;
 use 5.010001;
 use strict;
 use warnings;
-use Log::Any::IfLOG '$log';
 
+use App::GenPericmdCompleterScript qw(gen_perinci_cmdline_completer_script);
 use Bencher::ScenarioUtil::Completion qw(make_completion_participant);
-use File::Temp;
+use File::Temp qw(tempdir);
+use Perinci::CmdLine::Gen qw(gen_pericmd_script);
+
+my $tempdir;
 
 our $scenario = {
     summary => 'Benchmark completion response time, to monitor regression',
     modules => {
     },
     participants => [
-        make_completion_participant(
-            name=>'optname_common_help',
-            cmdline=>"CLI --hel^",
-        ),
-        make_completion_participant(
-            name=>'optname_common_version',
-            cmdline=>"CLI --vers^",
-        ),
-        make_completion_participant(
-            name=>'optname_action',
-            cmdline=>"CLI --acti^",
-        ),
-        make_completion_participant(
-            name=>'optval_action',
-            cmdline=>"CLI --action ^",
-        ),
     ],
+    before_list_participants => sub {
+        my %args = @_;
+
+        return if $tempdir;
+        my $keep = $ENV{DEBUG_KEEP_TEMPDIR} ? 1:0;
+        $tempdir = tempdir(CLEANUP => !$keep);
+
+        my $res;
+
+        my @cmds;
+
+        # XXX _oddeven
+
+        push @cmds, "oddeven-lite";
+        $res = gen_pericmd_script(
+            url => "/Perinci/Examples/Tiny/odd_even",
+            cmdline => "Perinci::CmdLine::Lite",
+            output_file => "$tempdir/oddeven-lite",
+        );
+        die "Can't create oddeven-lite: $res->[0] - $res->[1]"
+            unless $res->[0] == 200;
+
+        # XXX oddeven-lite-packed
+
+        push @cmds, "oddeven-classic";
+        $res = gen_pericmd_script(
+            url => "/Perinci/Examples/Tiny/odd_even",
+            cmdline => "Perinci::CmdLine::Classic",
+            output_file => "$tempdir/oddeven-classic",
+        );
+        die "Can't create oddeven-classic: $res->[0] - $res->[1]"
+            unless $res->[0] == 200;
+
+        my $sc = $args{scenario};
+        my $pp = $sc->{participants};
+
+        splice @$pp, 0;
+
+        for my $cmd (@cmds) {
+            push @$pp, make_completion_participant(
+                type => 'perl_code',
+                name=>"$cmd optname_common_help",
+                cmdline=>"$tempdir/$cmd --hel^",
+            );
+            push @$pp, make_completion_participant(
+                type => 'perl_code',
+                name=>"$cmd optname_common_version",
+                cmdline=>"$tempdir/$cmd --vers^",
+            );
+            push @$pp, make_completion_participant(
+                type => 'perl_code',
+                name=>"$cmd optname_number",
+                cmdline=>"$tempdir/$cmd --num^",
+            );
+            push @$pp, make_completion_participant(
+                type => 'perl_code',
+                name=>"$cmd optval_number",
+                cmdline=>"$tempdir/$cmd --number ^",
+            );
+        }
+
+        my $i = 0; for (@$pp) { $_->{seq} = $i++ }
+    },
     #datasets => [
     #],
 };
---
+++ lib/Bencher/Scenario/PerinciCmdLine/Runtime.pm
@@ -0,0 +1,107 @@
+package Bencher::Scenario::PerinciCmdLine::Runtime;
+
+# DATE
+# VERSION
+
+use 5.010001;
+use strict;
+use warnings;
+
+use App::GenPericmdCompleterScript qw(gen_perinci_cmdline_completer_script);
+use File::Temp qw(tempdir);
+use Perinci::CmdLine::Gen qw(gen_pericmd_script);
+
+my $tempdir;
+
+our $scenario = {
+    summary => 'Benchmark completion response time, to monitor regression',
+    modules => {
+    },
+    participants => [
+    ],
+    before_list_participants => sub {
+        my %args = @_;
+
+        return if $tempdir;
+        my $keep = $ENV{DEBUG_KEEP_TEMPDIR} ? 1:0;
+        $tempdir = tempdir(CLEANUP => !$keep);
+
+        my $res;
+
+        my @cmds;
+
+        push @cmds, "hello-inline";
+        $res = gen_pericmd_script(
+            url => "/Perinci/Examples/Tiny/hello_naked",
+            cmdline => "Perinci::CmdLine::Inline",
+            output_file => "$tempdir/hello-inline",
+        );
+        die "Can't create hello-inline: $res->[0] - $res->[1]"
+            unless $res->[0] == 200;
+
+        push @cmds, "hello-lite";
+        $res = gen_pericmd_script(
+            url => "/Perinci/Examples/Tiny/hello_naked",
+            cmdline => "Perinci::CmdLine::Lite",
+            output_file => "$tempdir/hello-lite",
+        );
+        die "Can't create hello-lite: $res->[0] - $res->[1]"
+            unless $res->[0] == 200;
+
+        # XXX hello-lite-packed
+
+        push @cmds, "hello-classic";
+        $res = gen_pericmd_script(
+            url => "/Perinci/Examples/Tiny/hello_naked",
+            cmdline => "Perinci::CmdLine::Classic",
+            output_file => "$tempdir/hello-classic",
+        );
+        die "Can't create hello-classic: $res->[0] - $res->[1]"
+            unless $res->[0] == 200;
+
+        my $sc = $args{scenario};
+        my $pp = $sc->{participants};
+
+        splice @$pp, 0;
+
+        for my $cmd (@cmds) {
+            push @$pp, {
+                type => 'perl_code',
+                name => "$cmd help",
+                summary => 'Run command --help',
+                code => sub {
+                    my $out = `$tempdir/$cmd --help`;
+                    die "Backtick failed: $?" if $?;
+                    $out;
+                }
+            };
+            push @$pp, {
+                type => 'perl_code',
+                name => "$cmd version",
+                summary => 'Run command --version',
+                code => sub {
+                    my $out = `$tempdir/$cmd --version`;
+                    die "Backtick failed: $?" if $?;
+                    $out;
+                }
+            };
+            push @$pp, {
+                type => 'perl_code',
+                name => "$cmd",
+                summary => 'Run command',
+                code => sub {
+                    my $out = `$tempdir/$cmd`;
+                    die "Backtick failed: $?" if $?;
+                    $out;
+                }
+            };
+        }
+
+        my $i = 0; for (@$pp) { $_->{seq} = $i++ }
+    },
+    #datasets => [
+    #],
+};
+
+1;
+# ABSTRACT:
